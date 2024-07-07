4c034dfeff165801e4945df172d82d8133850ff8
dev-mori <kierurorentu@gmail.com>
2024-07-08 00:23:44 +0900

    multi term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,9 +6,9 @@ return {
       --[[ things you want to change go here]]
     },
     keys = {
-      { "<leader>v", "<cmd>ToggleTerm direction=vertical size=40<cr>", desc = "toggleterm vertical" },
-      { "<leader>h", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "toggleterm horizontal" },
-      { "<leader>i", "<cmd>ToggleTerm direction=float<cr>", desc = "toggleterm float" },
+      { "<leader>v", "<cmd>ToggleTerm 1 direction=vertical size=40 name=v<cr>", desc = "toggleterm vertical" },
+      { "<leader>h", "<cmd>ToggleTerm 2 direction=horizontal name=h<cr>", desc = "toggleterm horizontal" },
+      { "<leader>i", "<cmd>ToggleTerm 3 direction=float name=i<cr>", desc = "toggleterm float" },
     },
   },
 }
