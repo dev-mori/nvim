8c1487a32a7f41eb9f42d18ad4fd3214353123ea
dev-mori <kierurorentu@gmail.com>
2024-09-09 12:25:26 +0900

    which-key 一次対応

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,6 +1,9 @@
 return {
   {
     "folke/which-key.nvim",
+    -- https://github.com/folke/which-key.nvim/issues/809
+    -- bug回避のためversion指定
+    version = "3.10.0",
     event = "VeryLazy",
     init = function()
       vim.o.timeout = true
