986ceef442d16a448cd7e99abe4df68d02fe8ca9
dev-mori <kierurorentu@gmail.com>
2024-11-27 15:41:12 +0900

    add lazygit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -8,5 +8,10 @@ return {
     { "<leader>v", "<cmd>ToggleTerm 1 direction=vertical size=40 name=v<cr>", desc = "toggleterm vertical" },
     { "<leader>h", "<cmd>ToggleTerm 2 direction=horizontal name=h<cr>", desc = "toggleterm horizontal" },
     { "<leader>i", "<cmd>ToggleTerm 3 direction=float name=i<cr>", desc = "toggleterm float" },
+    {
+      "<leader>g",
+      function() require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", direction = "float", count = 4, hidden = true }):toggle() end,
+      desc = "toggleterm lazygit",
+    },
   },
 }
