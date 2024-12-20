72855640ce5985cce332d716c8087babbcb1b4e7
dev-mori <kierurorentu@gmail.com>
2024-12-21 02:35:45 +0900

    add ruby-lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require('lspconfig')
+    lspconfig.ruby_lsp.setup({
+      cmd = { "ruby-lsp" },
+      filetypes = { "ruby" },
+      -- root_dir = lspconfig.util.root_pattern("Gemfile", ".git"),
+      root_dir = function(fname)
+        return lspconfig.util.root_pattern("Gemfile", ".git")(fname) or vim.fn.getcwd()
+      end,
+      init_options = {
+        formatting = "auto",
+      },
+      single_file_support = true,
+    })
+  end,
 }
