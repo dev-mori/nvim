d5733234290aac1324366e4b5f650f011ed1b9f7
dev-mori <kierurorentu@gmail.com>
2025-01-04 21:18:37 +0900

    add: global s

--- lua/config/apis.lua
+++ lua/config/apis.lua
@@ -3,3 +3,15 @@ local api = vim.api
 api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
   command = "checktime",
 })
+
+api.nvim_create_user_command("FindAndReplace", function(opts)
+  vim.api.nvim_command(string.format("cdo s/%s/%s", opts.fargs[1], opts.fargs[2]))
+  vim.api.nvim_command("cfdo update")
+end, { nargs = "*" })
+
+api.nvim_set_keymap(
+  "n",
+  "<leader>r",
+  ":FindAndReplace ",
+  { noremap = true }
+)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,5 +6,44 @@ return {
     { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "telescope live grep" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "telescope buffers" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "telescope help tags" },
+    { "<leader>fs", "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep For > ')})<cr>", desc = "telescope grep string" },
   },
+  config = function()
+    -- https://elanmed.dev/blog/global-find-and-replace-in-neovim
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+
+    local custom_actions = {}
+
+    function custom_actions.fzf_multi_select(prompt_bufnr)
+      local function get_table_size(t)
+        local count = 0
+        for _ in pairs(t) do
+          count = count + 1
+        end
+        return count
+      end
+
+      local picker = action_state.get_current_picker(prompt_bufnr)
+      local num_selections = get_table_size(picker:get_multi_selection())
+
+      if num_selections > 1 then
+        actions.send_selected_to_qflist(prompt_bufnr)
+        actions.open_qflist()
+      else
+        actions.file_edit(prompt_bufnr)
+      end
+    end
+
+    require('telescope').setup {
+      defaults = {
+        mappings = {
+          n = {
+            ["<cr>"] = custom_actions.fzf_multi_select,
+          },
+        },
+      },
+    }
+  end,
 }
