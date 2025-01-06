d9f5e5531f87db2859dc83cbcacc64317c71c5b4
dev-mori <kierurorentu@gmail.com>
2025-01-06 11:33:28 +0900

    add: line number

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,7 @@ opt.clipboard = "unnamedplus"
 opt.expandtab = true
 opt.foldlevel = 99
 opt.ignorecase = true
+opt.smartcase = true -- Don't ignore case with capitals
 opt.list = true -- Show some invisible characters (tabs...
 opt.scroll = 7 -- Jump 7 lines when scrolling
 opt.scrolloff = 10 -- Lines of context
@@ -12,7 +13,6 @@ opt.shiftwidth = 2 -- Size of an indent
 opt.showmode = false -- Dont show mode since we have a statusline
 opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
 opt.splitright = true -- Put new windows right current
 opt.splitbelow = true
@@ -28,6 +28,8 @@ opt.cursorlineopt = "number"
 opt.foldmethod = "syntax"
 opt.incsearch = true
 opt.autoread = true
+opt.number = true -- Show line numbers
+opt.relativenumber = true -- Relative line numbers
 
 -- if vim.fn.has("nvim-0.10") == 1 then
 --   opt.smoothscroll = true
