bcaeb8259e37661047082d8400a55b3086cfb169
dev-mori <kierurorentu@gmail.com>
2026-01-20 19:38:17 +0900

    comment: preview markdown

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,10 +1,10 @@
 -- For `plugins/markview.lua` users.
 return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  keys = {
-    { "<leader>pm", "<cmd>Markview splitToggle<cr>", desc = "markview split toggle" },
-  }
-  -- Completion for `blink.cmp`
-  -- dependencies = { "saghen/blink.cmp" },
+  -- "OXY2DEV/markview.nvim",
+  -- lazy = false,
+  -- keys = {
+  --   { "<leader>pm", "<cmd>Markview splitToggle<cr>", desc = "markview split toggle" },
+  -- }
+  -- -- Completion for `blink.cmp`
+  -- -- dependencies = { "saghen/blink.cmp" },
 };
